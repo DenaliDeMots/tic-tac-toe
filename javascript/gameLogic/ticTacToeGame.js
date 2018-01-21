c133b1a456f852a1459bfbc6949a17a3921654b1
DenaliDeMots <denali.demots@gmail.com>
2018-01-20 20:18:57 -0500

    Create exported message object for Tic Tac Toe game that represents all messages the ticTacToe object can emit.

--- javascript/gameLogic/ticTacToeGame.js
+++ javascript/gameLogic/ticTacToeGame.js
@@ -1,4 +1,39 @@
 const grid = require('./grid');
+const deepFreeze = require('deep-freeze');
+
+const valueAdded = grid.messages.setValueAt.valueAdded;
+const outOfBounds = grid.messages.setValueAt.coordinatesOutOfBounds;
+const alreadyUsed = grid.messages.setValueAt.coordinatesAlreadyUsed;
+const matchMessages = grid.messages.matchMessages;
+
+
+const messages = {
+    players: {
+        player1: 'player1',
+        player2: 'player2'
+    },
+    placeToken: {
+        player1PlacedToken: 'player1 placed token',
+        player2PlacedToken: 'player2 placed token'
+    },
+    placeTokenError: {
+        outOfBounds,
+        alreadyUsed
+    },
+    winMessages: {
+        matchMessages,
+        stalemate: 'stalemate'
+    }
+
+}
+deepFreeze(messages)
+
+const player1 = messages.players.player1;
+const player2 = messages.players.player2;
+const player1PlacedToken = messages.placeToken.player1PlacedToken;
+const player2PlacedToken = messages.placeToken.player2PlacedToken;
+const stalemate = messages.winMessages.stalemate;
+
 
 //object representing the rules of a Tic Tac Toe game
 function newTicTacToeGame(player1Token, player2Token) {
@@ -7,20 +42,20 @@ function newTicTacToeGame(player1Token, player2Token) {
 
     const gameBoard = grid.makeGrid(3, 3);
 
-    let turn = 'player1';
+    let turn = player1;
     let winnerMessage = false;
     
     function changeTurn () {
-        turn = turn === 'player1' ? 'player2' : 'player1'
+        turn = turn === player1 ? player2 : player1
     }
 
     function currentToken() {
-        return turn === 'player1' ? player1Token : player2Token
+        return turn === player1 ? player1Token : player2Token
     }
 
     function checkForWin () {
         let matchResults = checkForMatchingRowsColunmsOrDiagonals();
-        if(gameBoard.isGridFull() && matchResults.length === 0) return 'stalemate'
+        if(gameBoard.isGridFull() && matchResults.length === 0) return stalemate;
         return matchResults.length > 0 ? matchResults : false;
 
         function checkForMatchingRowsColunmsOrDiagonals() {
@@ -47,7 +82,7 @@ function newTicTacToeGame(player1Token, player2Token) {
             return generateGameMessage()
 
             function generateGameMessage() {
-                if (didPlaceToken === 'value added') {
+                if (didPlaceToken === valueAdded) {
                     return placeTokenResult()
                 } else {
                     return didPlaceToken;
@@ -59,7 +94,7 @@ function newTicTacToeGame(player1Token, player2Token) {
                 winnerMessage = checkForWin();
                 if(winnerMessage) return winnerMessage;
                 changeTurn();
-                return turn === 'player1' ? 'player2 placed token' : 'player1 placed token'
+                return turn === player1 ? player2PlacedToken : player1PlacedToken
             }
         }
     }
@@ -68,3 +103,4 @@ function newTicTacToeGame(player1Token, player2Token) {
 }
 
 exports.newTicTacToeGame = newTicTacToeGame;
+exports.messages = messages;
--- javascript/gameLogic/ticTacToeGame.test.js
+++ javascript/gameLogic/ticTacToeGame.test.js
@@ -1,46 +1,59 @@
 let ticTacToe = require('./ticTacToeGame');
 
-let player1 = {
+const msgs = ticTacToe.messages;
+
+const player1 = msgs.players.player1;
+const player2 = msgs.players.player2;
+const player1PlacedToken = msgs.placeToken.player1PlacedToken;
+const player2PlacedToken = msgs.placeToken.player2PlacedToken;
+const alreadyUsed = msgs.placeTokenError.alreadyUsed;
+const outOfBounds = msgs.placeTokenError.outOfBounds;
+const stalemate = msgs.winMessages.stalemate;
+const horizontal = msgs.winMessages.matchMessages.horizontalMatch;
+const vertical = msgs.winMessages.matchMessages.verticalMatch;
+const diagonal = msgs.winMessages.matchMessages.diagonalMatch;
+
+let token1 = {
     player: 'Bill',
     avater: './bill.png' 
 }
-let player2 = {
+let token2 = {
     player: 'Jessie',
     avatar: './jessie.png'
 }
 
 test('new game starts properly', () => {
-    let game = ticTacToe.newTicTacToeGame(player1, player2);
-    expect(game.getCurrentTurn()).toBe('player1')
+    let game = ticTacToe.newTicTacToeGame(token1, token2);
+    expect(game.getCurrentTurn()).toBe(player1)
     //game generates messages for placing tokens
     let message = game.placeToken(0, 0);
-    expect(message).toBe('player1 placed token')
+    expect(message).toBe(player1PlacedToken)
     message = game.placeToken(0, 2)
-    expect(message).toBe('player2 placed token')
+    expect(message).toBe(player2PlacedToken)
     message = game.placeToken(1, 1)
-    expect(message).toBe('player1 placed token')
+    expect(message).toBe(player1PlacedToken)
     //game does not allow placing a token on a taken space
     message = game.placeToken(0, 0)
-    expect(message).toBe('coordinates already used')
+    expect(message).toBe(alreadyUsed)
     //game does not allow an out of bounds move
     message = game.placeToken(0, 5)
-    expect(message).toBe('coordinates out of bounds')
+    expect(message).toBe(outOfBounds)
 })
 
 test('game ends when a player wins or when there are no moves left to play', () => {
-    let game = ticTacToe.newTicTacToeGame(player1, player2);
+    let game = ticTacToe.newTicTacToeGame(token1, token2);
     game.placeToken(0, 0);
     game.placeToken(2, 0);
     game.placeToken(1, 1);
     let message = game.placeToken(2, 1);
     //win message doesn't appear before a winning condition
-    expect(message).toBe('player2 placed token');
+    expect(message).toBe(player2PlacedToken);
     message = game.placeToken(2, 2);
     //win message appears on a winning condition
     let expectedWinMessage = [{
-        matchType: 'diagonal match',
-        value: player1,
-        startCorner: 'top left'
+        matchType: diagonal.matchType,
+        value: token1,
+        [diagonal.locationIdentifierKey]: diagonal.locations.topLeft
     }]
     expect(message).toEqual(expectedWinMessage);
     let gameState = game.getCurrentGameState();
@@ -66,5 +79,5 @@ test('game ends when a player wins or when there are no moves left to play', () 
     ]
     gameState = game.getCurrentGameState();
     expect(gameState).toEqual(expectedGameState);
-    expect(message).toBe('stalemate')
+    expect(message).toBe(stalemate)
 })
