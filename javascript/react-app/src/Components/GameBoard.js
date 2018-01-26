0168e53686d32e5f11dc071cf876b0143efd25c9
DenaliDeMots <denali.demots@gmail.com>
2018-01-25 20:58:06 -0500

    Add function and syling to draw cells in a 3 by 3 grid

--- javascript/react-app/src/Components/GameBoard.js
+++ javascript/react-app/src/Components/GameBoard.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Cell from './Cell';
 
-const GameBoard = ({gridArea}) => (
-    <div gridArea={gridArea}>
 
+//the area names represent the indices in a 2d array
+//the first number is the Y coordinate, the second is the X
+
+const style = {
+    display: 'grid',
+    gridTemplateAreas: `
+    "00" "01" "02"
+    "10" "11" "12"
+    "20" "21" "22"`,
+    gridTemplateColumns: "1fr 1fr 1fr",
+    gridTemplateRows: "1fr 1fr 1fr"
+}
+
+const GameBoard = ({gridArea, cells, board}) => (
+    <div gridArea={gridArea} style={style}>
+        {drawCells(board)}
     </div>
 )
+
+const drawCells = (board) => {
+    const cells = []
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board[0].length; j++){
+            let key = "" + i + j;
+            cells.push(<Cell gridArea={key} key={key} />)
+        }
+    }
+    return cells
+}
 
 export default GameBoard;
