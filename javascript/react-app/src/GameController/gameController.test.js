6d7b3f672089f0808615b0b7c800a110ba8e3861
DenaliDeMots <denali.demots@gmail.com>
2018-01-26 11:45:45 -0500

    Add test for game controller.  Failing due to build tool configuration

---
+++ javascript/react-app/src/GameController/gameController.test.js
@@ -0,0 +1,12 @@
+import gameController from './gameController';
+import store from './store';
+
+
+test('start single player game begins the game', () =>{
+    gameController.startSinglePlayerGame();
+    let newState = store.getState();
+    expect(newState.player).toBe('player1');
+    gameController.playMove({x: 0, y: 0})
+    newState = store.getState()
+    expect(newState.player).toBe('player2')
+})
