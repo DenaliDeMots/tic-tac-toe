857ea4bb0fa70b42c74c48cd9d9a905872806406
DenaliDeMots <denali.demots@gmail.com>
2018-01-25 20:35:13 -0500

    App draws the game board as a square taking up 90% of the smaller viewport dimention

--- javascript/react-app/src/Components/App.js
+++ javascript/react-app/src/Components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import GameBoard from './GameBoard';
 
+const style = {
+  display: 'grid',
+  gridTemplateAreas: `
+    ". . ."
+    ". gameBoard ."
+    ". . ."`,
+  gridTemplateColumns: '1fr 90vmin 1fr',
+  gridTemplateRows: '1fr 90vmin 1fr'
+}
 
 const App = () => (
-  <div>
-
+  <div style={style}>
+    <GameBoard gridArea={'gameBoard'}/>
   </div>
 )
 
