9e2bcf47ec81d814241a5f35f7843fb2c3f3e8fa
DenaliDeMots <denali.demots@gmail.com>
2018-01-25 23:28:21 -0500

    Remove default margin on body

--- javascript/react-app/src/index.js
+++ javascript/react-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import './index.css';
 import App from './Components/App';
 import reducer from './Reducer/reducer';
 
