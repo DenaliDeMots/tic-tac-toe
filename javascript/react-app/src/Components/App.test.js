a26bb7089387627aefb27233e14d174564137df1
DenaliDeMots <denali.demots@gmail.com>
2018-01-25 22:10:43 -0500

    Update app render test to account for react-redux provider

--- javascript/react-app/src/Components/App.test.js
+++ javascript/react-app/src/Components/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from '../Reducer/reducer';
 import App from './App';
+
+
+let store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+    <App />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
