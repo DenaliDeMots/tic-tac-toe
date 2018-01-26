722c5ee94d9000664bf720c2593fb771a9d8fc77
DenaliDeMots <denali.demots@gmail.com>
2018-01-26 00:14:54 -0500

    Create game controller object to wrap the tic tac toe game and dispatch update actions to redux

---
+++ javascript/react-app/src/GameController/gameController.js
@@ -0,0 +1,14 @@
+import ticTacToe from '../../../ticTacToe';
+import ai from '../../../ai/ticTacToe_HardAi';
+
+function makeGameController ({player1, player2}) {
+
+    const publicMethods = {
+
+    }
+    return publicMethods;
+}
+
+const gameController = makeGameController(ticTacToe.startTicTacToeGame('X', 'Y'));
+
+export default gameController;
