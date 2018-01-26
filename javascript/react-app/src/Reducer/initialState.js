d4cdf0b9a621715cf2757f7be1ce1a38d32a94bc
DenaliDeMots <denali.demots@gmail.com>
2018-01-25 19:39:25 -0500

    deep freeze the initial state

--- javascript/react-app/src/Reducer/initialState.js
+++ javascript/react-app/src/Reducer/initialState.js
@@ -1,13 +1,15 @@
+import deepFreeze from 'deep-freeze'
+
 const gameBoard = [
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
     [undefined, undefined, undefined]
 ]
 
-const INITIAL_STATE = {
+const INITIAL_STATE = deepFreeze({
     gameBoard,
     player: 'player1', // 'player1' || 'player2'
     sessionState: 'startMenu' // 'startMenu' || 'gameInProgress' || 'gameOver'
-}
+})
 
 export default INITIAL_STATE;
