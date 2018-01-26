adcdcb9cd44b22a09f0ceb84ccca2e2dc7398501
DenaliDeMots <denali.demots@gmail.com>
2018-01-26 00:33:44 -0500

    Add start game methods to  the game controller

--- javascript/react-app/src/GameController/gameController.js
+++ javascript/react-app/src/GameController/gameController.js
@@ -1,10 +1,23 @@
 import ticTacToe from '../../../ticTacToe';
 import ai from '../../../ai/ticTacToe_HardAi';
+import {startGame} from '../Actions/actions';
+import store from './store';
+import INITIAL_STATE from '../Reducer/initialState';
 
 function makeGameController ({player1, player2}) {
+    let gameType;
+    let currentPlaher = INITIAL_STATE.player;
 
     const publicMethods = {
+        startSinglePlayerGame(){
+            gameType = '1PlayerGame';
+            store.dispatch(startGame);
+        },
 
+        start2PlayerGame() {
+            gameType = '2PlayerGame'
+            store.dispatch(startGame);
+        }
     }
     return publicMethods;
 }
