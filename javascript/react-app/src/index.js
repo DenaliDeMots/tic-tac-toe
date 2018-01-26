3e289d008a9fed9d6110c74092d6cd9ba3303d33
DenaliDeMots <denali.demots@gmail.com>
2018-01-26 00:25:02 -0500

    Move the redux store into its own module

--- javascript/react-app/src/index.js
+++ javascript/react-app/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import './index.css';
+import store from './GameController/store';
 import App from './Components/App';
-import reducer from './Reducer/reducer';
 
-let store = createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+
   
   render(
     <Provider store={store}>
