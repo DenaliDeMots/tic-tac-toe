0f291568fe2afd5e0e69abb43377ebf80a1e911b
DenaliDeMots <denali.demots@gmail.com>
2018-01-27 12:27:33 -0500

    Register callback event on the player1 controller to manage turn change events

--- javascript/react-app/src/GameController/gameController.js
+++ javascript/react-app/src/GameController/gameController.js
@@ -3,15 +3,22 @@ import ai from '../../../ai/ticTacToe_HardAi';
 import {
     startGame,
     gameOver,
-    changePlayer
+    setPlayer
 } from '../Actions/actions';
 import store from './store';
 import INITIAL_STATE from '../Reducer/initialState';
 
 function makeGameController ({player1, player2}) {
     let gameType;
-    let currentPlayer = INITIAL_STATE.player;
+    let currentPlayer = player1;
     let moveResult;
+
+    player1.turnNotifier(onChangeTurn);
+
+    function onChangeTurn(player) {
+        currentPlayer = player.player;
+        store.dispatch(setPlayer(player.currentTurn))
+    }
 
     function dispatchMoveUpdate(){
         if(Array.isArray(moveResult)){
@@ -35,13 +42,7 @@ function makeGameController ({player1, player2}) {
         },
 
         playMove(move){
-            if(currentPlayer === 'player1'){
-                moveResult = player1.playMove(move)
-                dispatchMoveUpdate()
-            } else if (currentPlayer === 'player2') {
-                moveResult = player2.playMove(move)
-                dispatchMoveUpdate()
-            }
+            currentPlayer.playMove(move)
         }
     }
     return publicMethods;
--- javascript/react-app/src/GameController/gameController.test.js
+++ javascript/react-app/src/GameController/gameController.test.js
@@ -5,8 +5,8 @@ import store from './store';
 test('start single player game begins the game', () =>{
     gameController.startSinglePlayerGame();
     let newState = store.getState();
-    expect(newState.player).toBe('player1');
+    expect(newState.player).toBe('player 1');
     gameController.playMove({x: 0, y: 0})
     newState = store.getState()
-    expect(newState.player).toBe('player2')
+    expect(newState.player).toBe('player 2')
 })
